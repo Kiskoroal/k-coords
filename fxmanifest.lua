1a8ffb2e056e8dd571e1d1c10480e10b28547092
Francisco Rodríguez Alcón <111381119+Kiskoroal@users.noreply.github.com>
2026-02-01 12:59:17 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,14 +5,15 @@ author 'Kiskoroal'
 description 'Script que te devuelve la posición de tu PED, la finalidad de este script es facilitar el desarrollo de fivem'
 version '1.0.0'
 
-ui_page './ui/index.html'
+ui_page './html/index.html'
 
 files {
-    './ui/index.html',
-    './ui/styles.css',
-    './ui/script.js',
+    './html/index.html',
+    './html/styles.css',
+    './html/script.js',
 }
 
 client_scripts {
     './client/client.lua'
+
 }
